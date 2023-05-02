b08101955a086e88a35508986413dc3f78268375
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-02 19:03:41 +0300

    Starting run.py, first attempts

--- run.py
+++ run.py
@@ -4,4 +4,20 @@
 import os
 import requests
 
+#get env variable of user 
+user = os.getenv("USER")
+#find path of files
+path = ("/home/{}/supplier_description/".format(user))
+#find dir of files
+textDir = os.listdir(path)
+
+data = {}
+#process the files to create the data dir
+for file in textDir:
+	with open(path+file, "r") as openedFile:
+		lines = openedFile.readlines()
+		data["name"] = lines[0].strip("\n ")
+
+print(data)
+
 
