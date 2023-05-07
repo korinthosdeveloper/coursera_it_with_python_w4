da641c249c85526dc5adedbfc87481d10699113c
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-07 12:29:37 +0300

    First try to make report pdf file as the exercise

--- reports.py
+++ reports.py
@@ -8,9 +8,15 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 
+data = {"name": "banana",
+	"weight": "100 kg",
+	"desc": "good fruit"}
 
 styles = getSampleStyleSheet()
 report = SimpleDocTemplate("processed.pdf")
 report_title = Paragraph("Processed Update on ")
+report_body = Paragraph("ff")
 
-report.build([report_title])
+
+
+report.build([report_title, report_body])
