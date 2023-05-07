901537ea5dbfeee686504085c1cb5ddc7d4812b9
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-07 19:22:41 +0300

    get data and format the pdf body

--- report_email.py
+++ report_email.py
@@ -9,6 +9,17 @@ import reports
 attachment = "/tmp/processed.pdf"
 time_stamp = datetime.datetime.now().strftime("%Y-%m-%d")
 
+user = os.getenv("USER")
+path = ("/home/{}/supplier_description/".format(user))
+textDir = os.listdir(path)
+paragraph = ""
+
+for file in textDir:
+    with open(path+file, "r") as openedFile:
+        lines = openedFile.readlines()
+        paragraph += lines[0].strip("\n ") + " <br> " + lines[1].strip("\n ") + " <br> " 
+
+print(paragraph)
 
 if __name__ == "__main__":
     pass
