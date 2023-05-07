3eb026ae7464dd310e6ef0638c479c6098c37d56
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-07 19:06:55 +0300

    use datetime to get date

--- report_email.py
+++ report_email.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
-#the email send code here
+# make the report body here
+
+import os
+import datetime
+import reports
+
+attachment = "/tmp/processed.pdf"
+time_stamp = datetime.datetime.now().strftime("%Y-%m-%d")
+
+
+if __name__ == "__main__":
+    pass
+    #reports.generate_report(attachment, title, paragraph)
