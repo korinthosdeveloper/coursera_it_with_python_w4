fd703f5abc1c60e910e58884eb46e292cfa77604
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-07 12:13:25 +0300

    Create title to reports.py & firts pdf

--- reports.py
+++ reports.py
@@ -2,3 +2,15 @@
 
 #reports py to make the pdf report
 
+import datetime
+import os
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+
+
+styles = getSampleStyleSheet()
+report = SimpleDocTemplate("processed.pdf")
+report_title = Paragraph("Processed Update on ")
+
+report.build([report_title])
