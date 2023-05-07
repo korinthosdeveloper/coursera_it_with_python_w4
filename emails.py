82cfc5d7caf45b3e11d70138b1f841bf5a4cd890
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-07 19:58:40 +0300

    create the methods and imports / first steps

--- emails.py
+++ emails.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+from email.message import EmailMessage
+import mimetypes
+import smtplib
+
 # email methods and functions
+
+def generate_email():
+    pass
+
+def send_email():
+    pass
 
