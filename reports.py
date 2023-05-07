69586f4dfe3b71246aeb7b3e6da80f217b7c30c6
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-07 18:22:18 +0300

    create generate_report function

--- reports.py
+++ reports.py
@@ -6,15 +6,14 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 
-data = {"name": "banana",
-	"weight": "100 kg",
-	"desc": "good fruit"}
-
 styles = getSampleStyleSheet()
 report = SimpleDocTemplate("processed.pdf")
-report_title = Paragraph("Processed Update on ")
-report_body = Paragraph("ff")
+
+
+def generate_report(paragraph, title, file_path):
+    report_title = title
+    report_body = paragraph
+    attachment = file_path
 
 
 
-report.build([report_title, report_body])
