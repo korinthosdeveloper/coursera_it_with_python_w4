2b45dba6402a584d3da22a46e6ef6cf07b173592
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-07 18:37:01 +0300

    finalize the reports.py and use comments

--- reports.py
+++ reports.py
@@ -6,13 +6,21 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 
+#function to generate the pdf file
+#we call it from another file 
 
 def generate_report(paragraph, title, file_path):
-    styles = getSampleStyleSheet()
-    report_title = Paragraph(title)
-    report_body = Paragraph(paragraph)
+    # filename of pdf file
     report = SimpleDocTemplate(file_path)
-    report.buil([report_title,	report_body])
+    styles = getSampleStyleSheet()
+    # title of pdf
+    report_title = Paragraph(title, styles["h1"])
+    # main body
+    report_body = Paragraph(paragraph, styles["BodyText"])
+    # simple trick to have an empty line inside the pdf file
+    empty_line = Spacer(1, 20)
+    # create the pdf report file
+    report.buil([report_title, empty_line,report_body, empty_line])
 
 
 
