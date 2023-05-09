763dd14be03db8c1c4074ba94bcc7cd0e28c3da6
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-09 20:47:01 +0300

    fix images dir path and enable requests command

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -7,16 +7,16 @@ import os
 url = "http://localhost/upload/"
 #get user variable and make the files path
 user = os.getenv("USER")
-path = "/home/{}/images/modified_jpg/".format(user)
+path = "/home/{}/supplier-data/images/".format(user)
 
 files = os.listdir(path)
 
 for file in files:
 	if os.path.isfile(path+file):
-
+		# split name, ext
 		name, ext = os.path.splitext(file)
-		if ext == ".jpg":
+		# only upload .jpeg files
+		if ext == ".jpeg":
 			with open(path+name+ext, "rb") as file_binary:
-				print("The: {} is {} \n".format(file, file_binary))
-				#r = requests.post(url, files={"file": file_binary})
+				r = requests.post(url, files={"file": file_binary})
 
