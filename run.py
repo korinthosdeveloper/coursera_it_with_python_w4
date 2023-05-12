9571745bd0e2d49c585f1cf5ad4caad2491ec9df
Tsamanteros <tasos.tsamantas2015@gmail.com>
2023-05-12 18:39:03 +0300

    correct run.py 'descriptions' and 'jpeg'

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ url = "http://[linux-insta-ip]/fruits"
 #get env variable of user 
 user = os.getenv("USER")
 #find path of files
-path = ("/home/{}/supplier-data/description/".format(user))
+path = ("/home/{}/supplier-data/descriptions/".format(user))
 #find dir of files
 textDir = os.listdir(path)
 
@@ -28,7 +28,7 @@ for file in textDir:
 		# get weight with regex & convert it to int
 		data["weight"] = int(re.search("[0-9]*", lines[1].strip("\n "))[0])
 		data["description"] = lines[2].strip("\n ")
-		data["image_name"] = name+".jpg"
+		data["image_name"] = name+".jpeg"
 		#we want to upload json object to django server
 		r = requests.post(url, json= data)
 
